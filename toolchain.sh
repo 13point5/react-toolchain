a6bc2fa8c1d699cfc6bcfed0d93bc54bafd2f3cf
Bharath Sriraam R R <albharath.1305@gmail.com>
2020-10-20 10:35:12 +0530

    fix spacing issue in eslintrc

--- toolchain.sh
+++ toolchain.sh
@@ -29,8 +29,8 @@ echo
 echo "Which ESLint and Prettier configuration format do you prefer?"
 select config_extension in ".js" ".json" "Cancel"; do
   case $config_extension in
-    .js ) config_opening='module.exports = {'; break;;
-    .json ) config_opening='{'; break;;
+    .js ) config_opening='module.exports = '; break;;
+    .json ) config_opening=''; break;;
     Cancel ) exit;;
   esac
 done
